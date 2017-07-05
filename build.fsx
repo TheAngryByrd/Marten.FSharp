7c7d8c55b2def4815dd846bef90ccd49934231f6
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2017-07-05 17:11:14 -0400

    dotnet-mono no longer needs --restore for xplat projects

--- build.fsx
+++ build.fsx
@@ -16,8 +16,8 @@ Target "Clean" (fun _ ->
 
     !! srcGlob
     ++ testsGlob
-    |> Seq.collect(fun p -> 
-        ["bin";"obj"] 
+    |> Seq.collect(fun p ->
+        ["bin";"obj"]
         |> Seq.map(fun sp ->
              IO.Path.GetDirectoryName p @@ sp)
         )
@@ -34,7 +34,7 @@ Target "DotnetRestore" (fun _ ->
                 Project = proj
                 //This makes sure that Proj2 references the correct version of Proj1
                 AdditionalArgs = [sprintf "/p:PackageVersion=%s" release.NugetVersion]
-            }) 
+            })
 ))
 
 Target "DotnetBuild" (fun _ ->
@@ -45,7 +45,7 @@ Target "DotnetBuild" (fun _ ->
                 Project = proj
                 //This makes sure that Proj2 references the correct version of Proj1
                 AdditionalArgs = [sprintf "/p:PackageVersion=%s" release.NugetVersion]
-            }) 
+            })
 ))
 
 let whoami () =
@@ -64,10 +64,10 @@ type TargetFramework =
 
 let getTargetFramework tf =
     match tf with
-    | "net45" | "net451" | "net452" 
-    | "net46" | "net461" | "net462" -> 
+    | "net45" | "net451" | "net452"
+    | "net46" | "net461" | "net462" ->
         Full tf
-    | "netcoreapp1.0" | "netcoreapp1.1" -> 
+    | "netcoreapp1.0" | "netcoreapp1.1" ->
         Core tf
     | _ -> failwithf "Unknown TargetFramework %s" tf
 
@@ -79,24 +79,24 @@ let getTargetFrameworksFromProjectFile (projFile : string)=
     |> Seq.toList
 
 let selectRunnerForFramework tf =
-    let runMono = sprintf "mono --restore -f %s -c Release" 
+    let runMono = sprintf "mono -f %s -c Release"
     let runCore = sprintf "run -f %s -c Release"
     match tf with
     | Full t when isMono-> runMono t
     | Full t -> runCore t
     | Core t -> runCore t
-        
+
 
 let setEnvToIfNotExist key value =
-    
+
     match environVarOrNone key with
-    | None -> 
+    | None ->
         setEnvironVar key value
-        environVar key |> logfn "Env %s now set to %s \n" key  
-    | Some v -> 
-        v |> logfn "Env %s already set to %s \n" key 
-   
- 
+        environVar key |> logfn "Env %s now set to %s \n" key
+    | Some v ->
+        v |> logfn "Env %s already set to %s \n" key
+
+
 
 let setPostgresEnvVars () =
     //if environment variables aren't set, assume defaults
@@ -105,10 +105,10 @@ let setPostgresEnvVars () =
     setEnvToIfNotExist "POSTGRES_USER" (if isMacOS then (whoami ()) else "postgres")
     setEnvToIfNotExist "POSTGRES_PASS" "postgres"
     setEnvToIfNotExist "POSTGRES_DB" "postgres"
-    
+
 let runTests modifyArgs =
     setPostgresEnvVars ()
-    
+
     !! testsGlob
     |> Seq.map(fun proj -> proj, getTargetFrameworksFromProjectFile proj)
     |> Seq.collect(fun (proj, targetFrameworks) ->
@@ -129,7 +129,7 @@ Target "DotnetTest" (fun _ ->
 )
 let execProcAndReturnMessages filename args =
     let args' = args |> String.concat " "
-    ProcessHelper.ExecProcessAndReturnMessages 
+    ProcessHelper.ExecProcessAndReturnMessages
                 (fun psi ->
                     psi.FileName <- filename
                     psi.Arguments <-args'
@@ -167,13 +167,13 @@ Target "DotnetPack" (fun _ ->
                 Configuration = "Release"
                 OutputPath = IO.Directory.GetCurrentDirectory() @@ "dist"
                 AdditionalArgs = [sprintf "/p:PackageVersion=%s" release.NugetVersion]
-            }) 
+            })
     )
 )
 
 Target "Publish" (fun _ ->
     Paket.Push(fun c ->
-            { c with 
+            { c with
                 PublishUrl = "https://www.nuget.org"
                 WorkingDir = "dist"
             }
